e282c2cc450c5a4dccec3210fe0824d490584c2a
hockeyqc <flachance@hockey.qc.ca>
2026-07-16 16:08:09 -0400

    Ajouter les responsables régionaux

---
+++ site/regions-data.js
@@ -0,0 +1,76 @@
+const REGIONAL_CONTACTS = [
+  { region: "RSEQ", name: "Steve Gosselin", email: "sgosselin@rseq.ca" },
+  {
+    region: "Hockey Abitibi-T\xE9miscamingue",
+    name: "Marc D\xE9silets",
+    email: "marc.desilets@hockeyat.ca"
+  },
+  {
+    region: "Hockey Bas St-Laurent",
+    name: "Nancy Othot",
+    email: "nancy.othot@gmail.com"
+  },
+  {
+    region: "Hockey C\xF4te-Nord",
+    name: "Nancy Othot",
+    email: "nancy.othot@gmail.com"
+  },
+  {
+    region: "Hockey Estrie",
+    name: "Benoit Lejeune",
+    email: "benoit.lejeune@hockeyestrie.qc.ca"
+  },
+  {
+    region: "Hockey Gasp\xE9sie - Les \xCEles",
+    name: "Gabriel Vall\xE9e",
+    email: "vallee_91@hotmail.com"
+  },
+  {
+    region: "Hockey Lac St-Louis",
+    name: "Lyne Sauv\xE9",
+    email: "coord-adj@hockeylsl.ca"
+  },
+  {
+    region: "Hockey Laurentides-Lanaudi\xE8re",
+    name: "St\xE9phane Leblanc",
+    email: "stephane.leblanc@hockeyll.ca"
+  },
+  {
+    region: "Hockey Laval",
+    name: "Martine Deschamps",
+    email: "mdeschamps@hockeylaval.com"
+  },
+  {
+    region: "Hockey Mauricie",
+    name: "David Caron",
+    email: "directeur@hockeymauricie.ca"
+  },
+  {
+    region: "Hockey Qu\xE9bec r\xE9gion Montr\xE9al",
+    name: "St\xE9phanie Chartrand",
+    email: "registraire@hockeymontreal.org"
+  },
+  {
+    region: "Hockey Outaouais",
+    name: "Laudalina Santos",
+    email: "laudalinasantos3@gmail.com"
+  },
+  {
+    region: "Hockey Qu\xE9bec-Chaudi\xE8re-Appalaches",
+    name: "Nicolas Pelletier",
+    email: "npelletier@hockeyqca.ca"
+  },
+  {
+    region: "Hockey Richelieu",
+    name: "Virginie Sauv\xE9",
+    email: "virg.sauve@gmail.com"
+  },
+  {
+    region: "Hockey Saguenay Lac St-Jean",
+    name: "Andr\xE9 Dufour",
+    email: "registraire@hockey-saglac.com"
+  }
+];
+export {
+  REGIONAL_CONTACTS
+};
